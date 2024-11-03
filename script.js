559a3b59e5f122a36b9db2a4c6073b716a4edacb
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-11-03 11:23:36 +0400

    pricing component with toggle

--- script.js
+++ script.js
@@ -5,9 +5,20 @@
 
 
 const toggleBtn = document.querySelector(`.toggle--circle`);
-
+const priceArray = document.querySelectorAll(`.price`);
 
 
 toggleBtn.addEventListener(`click`, () => {
     toggleBtn.classList.toggle(`monthly`);
+
+    if (toggleBtn.classList.contains(`monthly`)) {
+        priceArray.forEach((el, i) => {
+            el.textContent = `${(String(parseInt(el.textContent)) + 9)}.99`
+        })
+    } else {
+        priceArray.forEach((el, i) => {
+            el.textContent = `${String(parseInt(el.textContent)).slice(0, -1)}.99`
+        })
+    }
+    
 })
